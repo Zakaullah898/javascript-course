d9eee480325d7dc29046718e9d5d82ae1dd33cdc
Zakaullah898 <zakaullahlaar9@gmail.com>
2025-02-22 16:00:41 +0000

    Div container

---
+++ basic1/test.js
@@ -0,0 +1,6 @@
+console.log('this is my first experience writting code in github');
+const studentName = 'Zaka'
+let roleNo = 12
+let seatNo = 5
+let className = '9Th'
+console.table([studentName,roleNo,seatNo,className])
